11fceb1a5b194dddc5dd2292f2dfe25117ac9ef8
ygrenzinger <yannick.grenzinger@gmail.com>
2018-07-25 22:03:52 +0200

    adding future monad

---
+++ src/main/kotlin/com/example/demo/FutureMonad.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.function.Supplier;
+
+public class FutureMonad<T> extends Monad<FutureMonad<T>, T> {
+
+    private Future<T> future;
+
+    private FutureMonad(Future<T> future) {
+        this.future = future;
+    }
+
+    public static <U> FutureMonad<U> instance(Future<U> future) {
+        return new FutureMonad<U>(future);
+    }
+
+    public static <U> FutureMonad<U> instance(U value) {
+        return new FutureMonad<U>(CompletableFuture.supplyAsync(() -> value));
+    }
+
+    @Override
+    public T get() {
+        try {
+            return future.get();
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+         
+    }
+
+    @Override
+    protected <U> Applicative<FutureMonad<U>, U> unit(U value) {
+        return instance(CompletableFuture.completedFuture(value));
+    }
+}
--- src/test/kotlin/com/example/demo/MonadDemoTest.java
+++ src/test/kotlin/com/example/demo/MonadDemoTest.java
@@ -3,24 +3,48 @@ package com.example.demo;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 public class MonadDemoTest {
 
     //Link : https://github.com/frankiesardo/monads
 
     @Test
     public void maybe_monad() {
-        Monad<?, String> stringMonad = Maybe.instance("string")
+        Monad<?, String> stringMonad = Maybe.instance("monad")
                 .map(String::toUpperCase)
                 .bind(s -> Maybe.instance(s.concat(" bind")));
-        Assert.assertEquals("STRING bind", stringMonad.get());
+        Assert.assertEquals("MONAD bind", stringMonad.get());
     }
 
     @Test
     public void maybe_monad_with_nothing() {
         Nothing<String> instance = Nothing.instance(String.class);
-        Maybe<String> stringMonad = Maybe.instance("string")
+        Monad<?, String>  stringMonad = Maybe.instance("string")
                 .bind(s -> instance)
-                .bind(s -> Maybe.instance(s.concat(" bind")));
-        Assert.assertEquals(instance, stringMonad);
+                .bind((String s) -> Maybe.instance(s.concat(" bind")));
+        Assert.assertEquals(instance, stringMonad);                   
+    }
+
+    private static <U> CompletableFuture<U> futureValue(U value) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Supplying value " + value);
+            return value;
+        });
+    }
+
+    @Test
+    public void future_monad() {
+        Monad<?, String> futureMonad = FutureMonad.instance(futureValue("MONAD"))
+                .map(String::toUpperCase)
+                .bind((String s) -> FutureMonad.instance(futureValue(s.concat(" bind"))));
+        Assert.assertEquals("MONAD bind", futureMonad.get());
     }
 }
